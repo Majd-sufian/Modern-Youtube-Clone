114bf298b43420fee508899f5f7d084bad1ffc06
Majd Sufyan <majdsufyan@Majds-MacBook-Pro.local>
2022-09-27 18:39:30 +0300

    Created a modern youtube clone 🚀

---
+++ src/components/Videos.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, Loader, VideoCard } from ".";
+
+interface VideosProps {
+  videos: any;
+  direction?: "row" | "row-reverse" | "column" | "column-reverse";
+}
+
+const Videos: React.FC<VideosProps> = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
+
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item: any, idx: number) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
